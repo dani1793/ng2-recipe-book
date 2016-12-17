5824f03df5ce9c422b1fd533e48e1a1517c70f2e
Daniyal Usmani <daniyal@smartfission.com>
2016-12-18 02:27:59 +0500

    header and list activation on selection

--- src/app/recipe/recipe-list/recipe-list.component.ts
+++ src/app/recipe/recipe-list/recipe-list.component.ts
@@ -1,27 +1,21 @@
-import {Component, OnInit, EventEmitter, Output} from '@angular/core';
-import { Recipe } from '../recipe';
-import { RecipeItemComponent } from './recipe-item.component'
+import {Component, OnInit} from '@angular/core';
+import {Recipe} from '../recipe';
+import {RecipeItemComponent} from './recipe-item.component'
 import {RecipeService} from "../recipe.service";
 
 @Component({
-  selector: 'rb-recipe-list',
-  templateUrl: './recipe-list.component.html'
+    selector: 'rb-recipe-list',
+    templateUrl: './recipe-list.component.html'
 })
 export class RecipeListComponent implements OnInit {
+    recipes: Recipe[] = [];
 
-  @Output() recipeSelected = new EventEmitter<Recipe>();
-  recipes: Recipe[] = [];
-  onSelected(recipe: Recipe){
 
-    this.recipeSelected.emit(recipe);
+    constructor(private recipeService: RecipeService) {
+    }
 
-  }
-
-  constructor(private recipeService: RecipeService) {
-  }
-
-  ngOnInit() {
-    this.recipes = this.recipeService.getRecipes();
-  }
+    ngOnInit() {
+        this.recipes = this.recipeService.getRecipes();
+    }
 
 }
